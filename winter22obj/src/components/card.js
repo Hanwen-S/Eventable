3e1ee84f23c4b440dde44f1b64e38180ef8de655
Haobo <yhbxhsuj@163.com>
2022-02-19 13:47:47 -0800

    add home, card, page, signIn, signUp, newnavbar components

---
+++ winter22obj/src/components/card.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+export default function BasicCard() {
+  return (
+    <Card style={{display: 'inline-block'}}>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Time
+            </Typography>
+            <Typography variant="h5" component="div">
+            Event_name
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Address
+            </Typography>
+            <Typography variant="body2">
+              Comment
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">More</Button>
+            <Button>Rating</Button>
+          </CardActions>
+    </Card>
+  );
+}
