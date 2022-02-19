630521f5f29c759a9ead9744e256b0f976b87bb3
Haobo <yhbxhsuj@163.com>
2022-02-19 14:25:52 -0800

    add more proototype components

--- winter22obj/src/index.js
+++ winter22obj/src/index.js
@@ -3,14 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-
+import SignIn from './components/signIn';
+import SignUp from "./components/signUp";
+import { Route } from "react-router-dom";
+import ResponsiveDrawer from "./components/home";
+import Checkout from './components/Checkout';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <Route exact path='/signup' component={SignUp}></Route>
+    <Route exact path='/' component={SignIn}></Route>
+    <Route exact path='/home' component={ResponsiveDrawer}></Route>
+    <Route exact path='/EventCard' component={Checkout}></Route>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
