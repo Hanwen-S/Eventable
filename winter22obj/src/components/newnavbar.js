72c35bf24b88561adcd36a0a2b5b5bc1309e2a96
Xinran F <xinranf59@gmail.com>
2022-03-06 14:57:48 -0800

    Add search bar in navbar * can enter strings in search bar * shows help message when hover * search algorithm not implemented

--- winter22obj/src/components/newnavbar.js
+++ winter22obj/src/components/newnavbar.js
@@ -14,7 +14,11 @@ import MenuItem from '@mui/material/MenuItem';
 // import {useHistory } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Link from '@mui/material/Link';
-const pages = [['Home', '/home'], ['Search', '/'], ['Timeslots', '/slothome'], ['Create', '/CreateEventForm']];
+import TextField from '@mui/material/TextField';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+const pages = [['Home', '/home'], ['Timeslots', '/slothome'], ['Create', '/CreateEventForm']];
 const settings = [['Profile', ''], 'Account', 'Dashboard', 'Logout'];
 
 
@@ -46,7 +50,52 @@ const ResponsiveAppBar = () => {
     let path = `/account`; 
     navigate(path);
   }
+
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
   
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '15ch',
+        '&:focus': {
+          width: '22ch',
+        },
+      },
+    },
+  }));
+  
+
+
 
   return (
     <AppBar position="static">
@@ -112,6 +161,20 @@ const ResponsiveAppBar = () => {
               </Button>
             ))}
           </Box>
+
+          
+          <Tooltip title= "Enter user or event ID" placement="bottom-start">
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          </Tooltip>
+          <Box sx={{ flexGrow: 0.5 }} />
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
