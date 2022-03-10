a29d7a2a34d8b60af43251a28ad5b9a1449b096d
weihanhowardqu <78530053+weihanhowardqu@users.noreply.github.com>
2022-03-10 17:27:23 +0800

    Potential Time Slot Histogram First Implementation

--- winter22obj/src/components/selfCard.js
+++ winter22obj/src/components/selfCard.js
@@ -25,6 +25,10 @@ export default function SelfCard(props) {
   const item = props.it;
   const [isOpen, setIsOpen] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
+  let array = [];
+  for (var i = 0; i < 48; i++) {
+    array[i] = 0;
+  }
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
@@ -32,6 +36,27 @@ export default function SelfCard(props) {
 
   const handleClick = () => {
     setIsOpen2(!isOpen2);
+    for (var i = 0; i < item.participants_id.length; i++) {
+      const myobj = {
+        user_id: item.participants_id[i],
+      };
+      axios
+      .get("http://localhost:5000/time_slots/get", {
+        params: myobj
+      })
+      .then((res) => {
+        console.log(i);
+        for (var j = 0; j < res.data.length; j++) {
+          if (res.data[j].year == item.date.substring(0, 4) && res.data[j].month == item.date.substring(5, 7) && res.data[j].day == item.date.substring(8, 10)) {
+            var start_index = res.data[j].start_index;
+            var end_index = res.data[j].end_index;
+            for (var k = start_index; k <= end_index; k++) {
+              array[k]++;
+            }
+          }
+        }
+      })
+    }
   }
 
   const Edit = () => {
@@ -72,6 +97,7 @@ export default function SelfCard(props) {
           '../home',
        ));
       })
+      console.log(array);
   };
 
 
@@ -124,8 +150,7 @@ export default function SelfCard(props) {
           <p>Address: {item.address}</p>
           <p>Creator: {item.creator_name}</p>
           <p>Participants: {item.participants_name}</p>
-          {/* <p>Participants: {item.event_name}</p>
-          <p>Has Passed: {item.status}</p> */}
+          {/*<p>Has Passed: {item.status}</p> */}
           <p>Description: {item.description}</p>
           <button className="timeSlotButton" onClick={handleClick}>Potential Time Slots <GrFormView /> </button>
         </>}
@@ -137,11 +162,11 @@ export default function SelfCard(props) {
         content={<>
           <div>
             <Histogram
-              xLabels={['2016', '2017', '2018']}
-              yValues={[324, 45, 672]}
-              width='410'
+              xLabels={['0:00', '0:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30']}
+              yValues={[array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9], array[10], array[11], array[12], array[13], array[14], array[15], array[16], array[17], array[18], array[19], array[20], array[21], array[22], array[23], array[24], array[25], array[26], array[27], array[28], array[29], array[30], array[31], array[32], array[33], array[34], array[35], array[36], array[37], array[38], array[39], array[40], array[41], array[42], array[43], array[44], array[45], array[46], array[47]]}
+              width='2000'
               height='450'
-              options={{ fillColor: '#FFFFFF', strokeColor: '#0000FF' }}
+              options={{ fillColor: '#FFFFFF', strokeColor: '#000000' }}
             />
           </div>
         </>}
